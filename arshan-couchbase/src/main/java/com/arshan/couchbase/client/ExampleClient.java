cdd38ecb463ae00fc6754c9aa697b0fa57a0b833
mas222r@gmail.com <mas222r@gmail.com>
2017-01-05 23:02:57 +0000

    bucket retrieval using N1QL QUERY

--- arshan-couchbase/src/main/java/com/arshan/couchbase/client/ExampleClient.java
+++ arshan-couchbase/src/main/java/com/arshan/couchbase/client/ExampleClient.java
@@ -1,85 +1,103 @@
 package com.arshan.couchbase.client;
+
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
-import com.arshan.couchbase.model.Rant;
-import com.couchbase.client.java.*;
-import com.couchbase.client.java.document.*;
-import com.couchbase.client.java.document.json.*;
-import com.couchbase.client.java.env.CouchbaseEnvironment;
-import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
-import com.couchbase.client.java.query.*;
-import com.google.gson.Gson;
-
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.arshan.couchbase.model.Rant;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonArray;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.env.CouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
+import com.couchbase.client.java.query.N1qlParams;
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.client.java.query.N1qlQueryResult;
+import com.couchbase.client.java.query.N1qlQueryRow;
+import com.couchbase.client.java.query.ParameterizedN1qlQuery;
+import com.couchbase.client.java.query.SimpleN1qlQuery;
+import com.couchbase.client.java.query.consistency.ScanConsistency;
+import com.google.gson.Gson;
+
 public class ExampleClient {
 
+	// private Bucket bucket;
+
 	public static void main(String[] args) {
-		
+
 		Logger logger = Logger.getLogger("com.couchbase.client");
 		logger.setLevel(Level.FINEST);
-		for(Handler h : logger.getParent().getHandlers()) {
-		    if(h instanceof ConsoleHandler){
-		        h.setLevel(Level.FINEST);
-		    }
+		for (Handler h : logger.getParent().getHandlers()) {
+			if (h instanceof ConsoleHandler) {
+				h.setLevel(Level.FINEST);
+			}
 		}
 
-		
 		CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder()
-			    //this set the IO socket timeout globally, to 45s
-			    .socketConnectTimeout((int) TimeUnit.SECONDS.toMillis(45))
-			    //this sets the connection timeout for openBucket calls globally (unless a particular call provides its own timeout)
-			    .connectTimeout(TimeUnit.SECONDS.toMillis(60))
-			    .build();
+				// this set the IO socket timeout globally, to 45s
+				.socketConnectTimeout((int) TimeUnit.SECONDS.toMillis(45))
+				// this sets the connection timeout for openBucket calls
+				// globally (unless a particular call provides its own timeout)
+				.connectTimeout(TimeUnit.SECONDS.toMillis(60)).build();
 		// Initialize the Connection
-        Cluster cluster = CouchbaseCluster.create(env, "127.0.0.1");
-        //System.out.println(cluster.clusterManager("Administrator", "password").info().raw());
+		Cluster cluster = CouchbaseCluster.create(env, "127.0.0.1");
+		// System.out.println(cluster.clusterManager("Administrator",
+		// "password").info().raw());
 
-        Bucket bucket = cluster.openBucket("customer360");
-        System.out.println("connected");
-        Gson gson = new Gson();
-		Rant rant = new Rant(UUID.randomUUID(),"Arshan","It's name dude","rant");
+		Bucket bucket = cluster.openBucket("customer360");
+		System.out.println("connected");
+		Gson gson = new Gson();
+		Rant rant = new Rant(UUID.randomUUID(), "ArshanM", "It's name dude", "rant");
 		JsonObject abdulSuboor = JsonObject.create().fromJson(gson.toJson(rant));
 		bucket.upsert(JsonDocument.create(rant.getId().toString(), abdulSuboor));
-        
+		
+		bucket.bucketManager().createN1qlPrimaryIndex(true, false);
+			
+		resultFromSimpleQuery(bucket);
+
+		allResultsFromBucket(bucket);
 		// Just close a single bucket
 		bucket.close();
 
 		// Disconnect and close all buckets
 		cluster.disconnect();
-
-       /* // Create a JSON Document
-        JsonObject abdulSuboor = JsonObject.create()
-            .put("name", "AbdulSuboor")
-            .put("email", "mas222r@gmail.com")
-            .put("interests", JsonArray.from("Holy Grail", "African Swallows"));
-
-        // Store the Document
-        bucket.upsert(JsonDocument.create("u:mas_222r", abdulSuboor));
-       
-        // Load the Document and print it
-        // Prints Content and Metadata of the stored Document
-        System.out.println(bucket.get("u:mas_222r"));
-
-        // Create a N1QL Primary Index (but ignore if it exists)
-        bucket.bucketManager().createN1qlPrimaryIndex(true, false);
-
-        // Perform a N1QL Query
-        N1qlQueryResult result = bucket.query(
-            N1qlQuery.parameterized("SELECT name FROM default WHERE $1 IN interests",
-            JsonArray.from("African Swallows"))
-        );
-
-        // Print each found Row
-        for (N1qlQueryRow row : result) {
-            // Prints {"name":"Arthur"}
-            System.out.println(row);
-        }*/
-    }
-
+		
 	}
 
+	private static void allResultsFromBucket(Bucket bucket) {
+		JsonDocument doc;
+		String statement = "SELECT * FROM customer360 WHERE username = $1";
+		JsonArray values = JsonArray.empty().add("ArshanM");
+		N1qlParams params = N1qlParams.build().consistency(ScanConsistency.REQUEST_PLUS);
+		ParameterizedN1qlQuery query = ParameterizedN1qlQuery.parameterized(statement, values, params);
+		N1qlQueryResult result = bucket.query(query);
+		List<N1qlQueryRow> list = result.allRows();
+		System.out.println(list.size());
+		if (list.size() != 0) {
+			for (N1qlQueryRow n1qlQueryRow : list) {
+				System.out.println(n1qlQueryRow);
+			}
+		}
+	}
+	
+	private static void resultFromSimpleQuery(Bucket bucket)
+	{
+		SimpleN1qlQuery simpleN1q1Query = N1qlQuery.simple("SELECT * FROM customer360");		
+		N1qlQueryResult result = bucket.query(simpleN1q1Query);
+		List<N1qlQueryRow> list = result.allRows();
+		System.out.println("Simple Query result count : "+list.size());
+		if (list.size() != 0) {
+			for (N1qlQueryRow n1qlQueryRow : list) {
+				System.out.println(n1qlQueryRow);
+			}
+		}
+	}
+
+}
